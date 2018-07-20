b2066dec0d9c6771e0b8c966d4a23d2c2ba26459
Jasen Green <jasen.green@protonmail.com>
2018-07-20 07:49:55 -0700

    Add files via upload

---
+++ task.js
@@ -0,0 +1,29 @@
+const shell = require('node-powershell');
+const cron = require('node-cron'); 
+const winston = require('winston');
+const moment = require('moment');
+const time = () => moment().format('YYYY-MM-DD hh:mm:ss').trim();
+
+
+winston.add(winston.transports.File, { filename: 'NodeShell.log', timestamp: time });
+//Task Scheduler
+cron.schedule('0 7 * * *', function () {
+
+let ps = new shell({
+  executionPolicy: 'Bypass',
+  noProfile: true
+});
+//Run PowerShell Script
+ps.addCommand('C:/Users/jgreen-a/Desktop/node/ps/missing');
+
+ps.invoke()
+.then(output => {
+ console.log(output);
+  })
+.catch(err => {
+  console.log(err);
+  ps.dispose();
+});
+ //Logging
+winston.log('info', 'Process Ran');
+});
