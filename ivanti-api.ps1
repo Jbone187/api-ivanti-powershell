3abed7d1993a0577f00143261dc698571a3a0bd2
Jasen Green <jasen.green@protonmail.com>
2018-07-20 07:28:22 -0700

    Add files via upload

---
+++ ivanti-api.ps1
@@ -0,0 +1,33 @@
+#Access Api
+Import-Module STProtect
+
+#Load Groups from Ivanti
+$Groups = Get-Content -Path 
+
+#Scan Group of Servers
+foreach($i in $Groups) 
+{
+
+#Patching Code
+$Patching = Start-PatchScan -TemplateName "Scan" –MachineGroups $i
+$Patching | Watch-PatchScan -ErrorAction silentlycontinue
+$Patching | Wait-PatchScan
+$Patching | Start-PatchDeploy -TemplateName "ManualReboot"
+
+
+$Results = $Patching | Watch-PatchScan
+$Missing = $Results | Where {($_.MissingPatches -gt 0)} | Select Name, MissingPatches 
+ 
+#Results of Scan
+$Missing | Out-File ''
+$Results  | Out-File -FilePath 
+#Get Scan Data
+$ScanInfo = Get-Content -Path 
+#Piped Results
+$BodyInfo = $ScanInfo | Out-String
+ #Send Email with Scan Results
+$recipients = @("")
+$To = $recipients
+$message = "$i `n Servers Missing Patches`n $BodyInfo "
+Send-MailMessage -To $To -from  -Subject 'Ivanti Patch Scan' -Body $message  -SmtpServer smtp.cdicorp.com
+}
